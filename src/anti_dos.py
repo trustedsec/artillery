3fa8b5d844f6c79dacfde2dfdbeac9d1305f3d49
Mark Gaulton <markgaulton@gmail.com>
2015-01-21 14:20:48 -0500

    dupe check for antidos

--- src/anti_dos.py
+++ src/anti_dos.py
@@ -11,7 +11,16 @@ anti_dos_throttle = read_config("ANTI_DOS_THROTTLE_CONNECTIONS")
 anti_dos_burst = read_config("ANTI_DOS_LIMIT_BURST")
 
 if is_config_enabled("ANTI_DOS"):
-    # basic throttle for some ports
-    anti_dos_ports = anti_dos_ports.split(",")
-    for ports in anti_dos_ports:
-        subprocess.Popen("iptables -A INPUT -p tcp --dport %s -m limit --limit %s/minute --limit-burst %s -j ACCEPT" % (ports,anti_dos_throttle,anti_dos_burst), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+	# basic throttle for some ports
+	anti_dos_ports = anti_dos_ports.split(",")
+	for ports in anti_dos_ports:
+		proc = subprocess.Popen("iptables -vnL INPUT | grep limit", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		checker = proc.stdout.readlines()
+		matched = 'no'
+		portcheck = "tcp dpt:%s limit" %(ports)
+		if any( portcheck in s for s in checker):
+			matched = 'yes'
+			#print 'rule found'
+		if matched == 'no':
+			#print 'Rule not found'
+			subprocess.Popen("iptables -A INPUT -p tcp --dport %s -m limit --limit %s/minute --limit-burst %s -j ACCEPT" % (ports,anti_dos_throttle,anti_dos_burst), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
